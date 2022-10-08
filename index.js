0e3e1135fc49bbcbd35e6c8486ce9a5eafc341f8
Paige <prhoward97@gmail.com>
2022-10-08 14:18:27 -0400

    commit tree 1ff0218e18c1948ce776979efa866201c0ce225f

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+const totalBatteries = batteryBatches.reduce((previousValue, currentValue) => previousValue + currentValue);
+        
 // Code your solution here
+/*function batteries() {
+     let initialValue = 0;
+     
+     return totalBatteries;
+
+}*/
